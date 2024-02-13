3ca4091e70547f90cce1b924543be4c6cd84616c
Islam <i_dyussenov@kbtu.kz>
2024-02-13 09:45:28 +0600

    1

---
+++ Lab1/Variables/1_Variables.py
@@ -0,0 +1,21 @@
+"""
+Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it.
+"""
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+#Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+Example
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
---
+++ Lab1/Variables/4_OutputVars.py
@@ -0,0 +1,49 @@
+Output Variables
+The Python print() function is often used to output variables.
+
+
+x = "Python is awesome"
+print(x)
+
+
+
+
+In the print() function, you output multiple variables, separated by a comma:
+
+Example
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+
+You can also use the + operator to output multiple variables:
+
+Example
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+
+Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+
+For numbers, the + character works as a mathematical operator:
+
+Example
+x = 5
+y = 10
+print(x + y)
+
+
+
+The best way to output multiple variables in the print() function is to separate them with commas,
+which even support different data types:
+
+Example
+x = 5
+y = "John"
+
+print(x, y)
